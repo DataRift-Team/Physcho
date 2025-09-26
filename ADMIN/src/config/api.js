67bbad0bfef7c3256523622a73ab936e0d51098f
Manu Omar <107280248+Manuomar@users.noreply.github.com>
2025-09-26 14:45:46 +0530

    Update api.js

--- ADMIN/src/config/api.js
+++ ADMIN/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 // API base URL - update this to your backend URL
-const API_BASE_URL = "http://localhost:8000/api"
+const API_BASE_URL = "https://physcho.onrender.com/api"
 
 // Create axios instance
 const api = axios.create({
